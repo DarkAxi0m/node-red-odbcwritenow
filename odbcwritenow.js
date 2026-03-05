a110a60f0d0552f23f71ce7360732c0ca1791a7d
Chris Chase <chris@accede.com.au>
2026-03-05 11:56:59 +1030

    restet

--- odbcwritenow.js
+++ odbcwritenow.js
@@ -55,7 +55,7 @@ async function DoImport(msg, url, node, maxRetries, baseBackoffMs) {
 
             node.status({ fill: "green", shape: "dot", text: `#${msg.page}: ${msg.rows} Rows` })
             msg.payload = data;
-            return node.send([{ "rows": msg.rows }, null]);
+            return node.send([msg, null]);
         } catch (error) {
             node.status({ fill: "red", shape: "ring", text: error.message })
             console.error(error.message)
